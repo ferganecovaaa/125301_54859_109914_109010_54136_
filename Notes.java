d9c4434fb8024d720e0b937e16ebdd0dbddb26d7
ferganecovaaa <fergenacovaaaaa@gmail.com>
2024-06-17 19:03:54 +0200

    Added text alignment and different text colors

--- Notes.java
+++ Notes.java
@@ -42,6 +42,18 @@ public class Notes extends JFrame {
         controlPanel.add(new JLabel("Font Size:"));
         controlPanel.add(fontSizeComboBox);
 
+        fontColorComboBox = new JComboBox<>(new String[]{"Black", "Red", "Green", "Blue", "White"});
+        fontColorComboBox.setSelectedIndex(0);
+        fontColorComboBox.addActionListener(new FontColorAction());
+        controlPanel.add(new JLabel("Font Color:"));
+        controlPanel.add(fontColorComboBox);
+
+        textAlignComboBox = new JComboBox<>(new String[]{"Left", "Center", "Right"});
+        textAlignComboBox.setSelectedIndex(0);
+        textAlignComboBox.addActionListener(new TextAlignAction());
+        controlPanel.add(new JLabel("Text Align:"));
+        controlPanel.add(textAlignComboBox);
+
         panel.add(controlPanel, BorderLayout.NORTH);
         add(panel);
 
@@ -69,6 +81,39 @@ public class Notes extends JFrame {
             SimpleAttributeSet attributeSet = new SimpleAttributeSet();
             StyleConstants.setFontSize(attributeSet, size);
             textPane.setCharacterAttributes(attributeSet, false);
+        }
+    }
+
+    private class FontColorAction implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String colorStr = (String) fontColorComboBox.getSelectedItem();
+            Color color = switch (colorStr) {
+                case "Red" -> Color.RED;
+                case "Green" -> Color.GREEN;
+                case "Blue" -> Color.BLUE;
+                case "White" -> Color.WHITE;
+                default -> Color.BLACK;
+            };
+            SimpleAttributeSet attributeSet = new SimpleAttributeSet();
+            StyleConstants.setForeground(attributeSet, color);
+            textPane.setCharacterAttributes(attributeSet, false);
+        }
+    }
+
+    private class TextAlignAction implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String alignStr = (String) textAlignComboBox.getSelectedItem();
+            int alignment = switch (alignStr) {
+                case "Center" -> StyleConstants.ALIGN_CENTER;
+                case "Right" -> StyleConstants.ALIGN_RIGHT;
+                default -> StyleConstants.ALIGN_LEFT;
+            };
+            StyledDocument doc = textPane.getStyledDocument();
+            SimpleAttributeSet attributeSet = new SimpleAttributeSet();
+            StyleConstants.setAlignment(attributeSet, alignment);
+            doc.setParagraphAttributes(0, doc.getLength(), attributeSet, false);
         }
     }
 
